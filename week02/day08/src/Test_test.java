c0214a1322860cd4c3f5dbfffc943cac4fe2e120
FallCicada <3471437753@qq.com>
2024-09-04 16:28:27 +0800

    2024/9/4数组上传

--- week02/day08/src/Test_test.java
+++ week02/day08/src/Test_test.java
@@ -3,7 +3,7 @@
  * @Author: FallCicada
  * @Date: 2024-09-04 14:54:56
  * @LastEditors: FallCicada
- * @LastEditTime: 2024-09-04 15:08:53
+ * @LastEditTime: 2024-09-04 16:22:02
  */
 import java.util.Arrays;
 import java.util.Scanner;
@@ -21,6 +21,7 @@ public class Test_test {
         }
         System.out.println("您生成的数组Array = " + Arrays.toString(arr));
         arrAvg(arr);
+        System.out.println("您输入的数组的最大值为 = " + arrMax(arr));
     }
     static void arrAvg(int[] arr){
         double sum = 0;
@@ -28,5 +29,23 @@ public class Test_test {
             sum += arr[i];
         }
         System.out.println("您输入数组的平均值 = " + sum/arr.length);
+    }
+    static int arrMax(int[] arr){
+        // 定义一个变量，用于记录最大值
+        int max = arr[0];
+        // 定义一个索引变量，用于记录最大值在数组中的索引
+        int index = 0;
+        // 循环遍历数组
+        for(int i = 1; i < arr.length; i++){
+            // 判断当前元素是否大于最大值
+            if(arr[i] > max){
+                // 将最大的数值赋给max
+                max = arr[i];
+            }
+            // 将最大值索引赋给index
+            index = i;
+        }
+        System.out.println("最大值下标为：" + "[" + (index + 1) + "]");
+        return max;
     }
 }
