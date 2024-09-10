54ffc10f37fa2e98e265613001cf55e2f2c64619
FallCicada <3471437753@qq.com>
2024-09-10 11:45:00 +0800

    push

---
+++ course/week03/day12/src/Duration/Duration.java
@@ -0,0 +1,55 @@
+/*
+ * @Description: 
+ * @Author: FallCicada
+ * @Date: 2024-09-10 11:27:25
+ * @LastEditors: FallCicada
+ * @LastEditTime: 2024-09-10 11:44:36
+ */
+package Duration;
+public class Duration {
+    int hour;
+    int min; 
+    int sec;
+
+    public Duration() {
+        
+    }
+
+    public Duration(int seconds) {
+        int hour = seconds/3600;
+        int min = (seconds%3600)/60;
+        int sec = (seconds%3600)%60;
+        this.hour = hour;
+        this.min = min;
+        this.sec = sec;
+    }
+
+    public Duration(int hour, int min, int sec) {
+        this.hour = hour;
+        this.min = min;
+        this.sec = sec;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getSec() {
+        return sec;
+    }
+
+    public int getTotalSeconds() {
+        return hour*3600 + min*60 + sec;
+    }
+
+    public void disp() {
+        int second = hour*3600 + min*60 + sec;
+        System.out.println("总秒数:" + second);
+    }
+
+
+}
