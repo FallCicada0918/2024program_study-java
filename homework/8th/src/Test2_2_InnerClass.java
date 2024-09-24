10d12a1940c6229a17848ead24c41e11f2c75bf0
FallCicada <3471437753@qq.com>
2024-09-24 17:27:37 +0800

    错"

--- homework/8th/src/Test2_2_InnerClass.java
+++ homework/8th/src/Test2_2_InnerClass.java
@@ -3,26 +3,24 @@
  * @Author: FallCicada
  * @Date: 2024-09-21 18:16:13
  * @LastEditors: FallCicada
- * @LastEditTime: 2024-09-21 18:16:39
+ * @LastEditTime: 2024-09-24 17:23:26
  */
-interface Inner {
-    void show();
-}
-
 class Outer {
-    // 返回一个实现了 Inner 接口的匿名内部类实例
+    // 使用内部类实现 show 方法
+    public static class Inner {
+        public void show() {
+            System.out.println("HelloWorld");
+        }
+    }
+
+    // 返回内部类的实例
     public static Inner method() {
-        return new Inner() {
-            @Override
-            public void show() {
-                System.out.println("HelloWorld");
-            }
-        };
+        return new Inner();
     }
 }
 
-public class Test2_2_InnerClass {
+class Test2_2_InnerClass {
     public static void main(String[] args) {
-        Outer.method().show(); // 输出 HelloWorld
+        Outer.method().show();
     }
 }
