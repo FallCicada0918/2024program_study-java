87ba288503e44987ba3243a0661adb04c62598bb
FallCicada <3471437753@qq.com>
2024-10-04 00:12:54 +0800

    代码提交测试

---
+++ course/self-study/07Code/src/HerlloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: FallCicada
+ * @Date: 2024-10-04 00:12:12
+ * @LastEditors: FallCicada
+ * @LastEditTime: 2024-10-04 00:12:16
+ */
+/**
+ * HerlloWorld
+ */
+public class HerlloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
