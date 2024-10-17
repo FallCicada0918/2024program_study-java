8a9b7cc677ed0e1bc0c0e3a1f566d5e96a3fbf83
FallCicada <3471437753@qq.com>
2024-10-17 11:45:21 +0800

    文档添加关于字符串、字节和void数据类型的说明
    在NumPy文档中，常规介绍部分现在包含了关于字符串、字节和void数据类型的信息，以及现有的数值类型。

--- homework/14th/src/Test03/Teacher.java
+++ homework/14th/src/Test03/Teacher.java
@@ -3,15 +3,19 @@
  * @Author: FallCicada
  * @Date: 2024-10-16 14:50:23
  * @LastEditors: FallCicada
- * @LastEditTime: 2024-10-16 19:45:53
+ * @LastEditTime: 2024-10-17 10:42:25
  * @: 無限進步
  */
 package Test03;
 
 import java.io.Serializable;
 
-
+//实现序列化接口,是为了让Teacher类对象可以被序列化和反序列化
 public class Teacher implements Serializable {
+    // 序列化版本ID
+    //标识当前类的版本,用于序列化和反序列化时的本本校验
+    private static final long serialVersionUID = 1L;
+    //确认序列化版本ID,序列化和序列化时都会检查这个ID,确认前后一致性
     private String name;
     private int age;
     private double salary;
